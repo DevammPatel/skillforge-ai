6cb17bbd7d6cfd7448d8480da677823571005c0a
Devamm Patel <pateldevamm.12@gmail.com>
2026-06-14 16:15:30 +0530

    Final modification after Foundry database integration

--- agents/learning_curator_agent.py
+++ agents/learning_curator_agent.py
@@ -1,29 +1,28 @@
 import os
 
-
 class LearningCuratorAgent:
 
     def __init__(self):
         self.docs_path = "knowledge_base"
 
     def get_learning_material(self):
-
         content = ""
 
-        for file in os.listdir(self.docs_path):
+        if os.path.exists(self.docs_path):
+            for file in os.listdir(self.docs_path):
 
-            if file.endswith(".md"):
+                if file.endswith(".md"):
 
-                with open(
-                    os.path.join(
-                        self.docs_path,
-                        file
-                    ),
-                    "r"
-                ) as f:
+                    with open(
+                        os.path.join(
+                            self.docs_path,
+                            file
+                        ),
+                        "r"
+                    ) as f:
 
-                    content += f.read()
+                        content += f.read()
 
-                    content += "\n\n"
+                        content += "\n\n"
 
         return content
